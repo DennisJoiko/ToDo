d55c62f59bc3dbf2e818aed493b635d8a6e59b6c
Avartos <59014772+Avartos@users.noreply.github.com>
2022-12-19 17:14:27 +0100

    done

--- scripts.js
+++ scripts.js
@@ -6,10 +6,14 @@ const todoList = document.getElementById("todoList")
 addButton.addEventListener("click", ()=>{
     let insert = todoInput.value
     let toDo = document.createElement("li");
-    toDo.innerHTML = insert;
+    toDo.classList.add('mdl-list__item')
+    toDo.innerHTML =  `<span class="mdl-list__item-primary-content">
+                        <i class="material-icons  mdl-list__item-avatar">add</i>
+                            ${insert}
+                        </span>`;
 
     let deleteButton = document.createElement("button");
-    deleteButton.innerText = "X";
+    deleteButton.innerHTML =`<i class="material-icons  mdl-list__item-avatar">remove</i>`;
 
     toDo.appendChild(deleteButton);
     todoList.appendChild(toDo);
